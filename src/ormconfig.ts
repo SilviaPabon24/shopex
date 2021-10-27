6581729d79d0160418def1066b3a75e968c339eb
SilviaPabon24 <silvia.juliana24@hotmail.com>
2021-10-27 17:24:53 -0500

    adding credentials

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -2,21 +2,21 @@ import { ConnectionOptions } from "typeorm";
 import {join} from "path";
 import { User } from "./auth/entity/user.entity";
 
-const config = {
-    host: "localhost",
-    user: "postgres",
-    password: "root",
-    database: "postgres",
-};
 
 const connectionOptions: ConnectionOptions = {
     type: "postgres",
-    host: "localhost",
+    url: "postgres://wsuuioeptfnqeg:d519b323fdadf5d6d2f435d04dc5d77daddc79a8c4d044efdcae520a0cdf3e4f@ec2-3-226-211-228.compute-1.amazonaws.com:5432/d6qabm2p48a4am",
+    host: "ec2-3-226-211-228.compute-1.amazonaws.com",
     port: 5432,
-    username: config.user,
-    password: config.password,
-    database: config.database || "postgres",
+    username: "wsuuioeptfnqeg",
+    password: "d519b323fdadf5d6d2f435d04dc5d77daddc79a8c4d044efdcae520a0cdf3e4f",
+    database: "d6qabm2p48a4am" || "postgres",
     entities: [User],
+    extra:{
+        ssl:{
+            rejectUnauthorized: false,
+        },
+    },
     synchronize: true,
     dropSchema: false,
     migrationsRun: true,
