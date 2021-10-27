6d0dbb680631c78db59158f9dd416de746c67324
SilviaPabon24 <silvia.juliana24@hotmail.com>
2021-10-27 17:27:53 -0500

    "added homeroute"

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,14 @@ import { Router } from "express";
 import { AuthController } from "./auth/auth_controller";
 
 const router = Router();
+
+//Default
+router.get("/user", (req,res) => {
+    res.send({
+        message : "API IS WORKING",
+    });
+});
+
 router.get("/user", AuthController.fetchUser);
 //singning in
 router.post("/user/signup", AuthController.signUp);
